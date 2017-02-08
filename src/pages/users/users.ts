2f80f35a3af787b89da45d372748cd1f6757a3e0
Taylor Mason <taylorwmason@gmail.com>
2017-02-07 20:45:54 -0500

    first commit

---
+++ src/pages/users/users.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { User } from '../../models/user';
+
+import { GithubUsers } from '../../providers/github-users'
+
+/*
+  Generated class for the Users page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html'
+})
+export class UsersPage {
+	users: User[]
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private githubUsers: GithubUsers) {
+  	githubUsers.load().subscribe(users => {
+  		console.log(users)
+  	})
+  }
+
+  ionViewDidLoad() {
+    console.log('Hello Users Page');
+  }
+
+}
